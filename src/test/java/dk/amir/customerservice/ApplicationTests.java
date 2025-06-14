e587611144d16f971f9fcd0423c516ca325a1ee6
amirholya01 <amirholya01@gmail.com>
2025-06-14 21:29:46 +0200

    first commit

---
+++ src/test/java/dk/amir/customerservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.amir.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
