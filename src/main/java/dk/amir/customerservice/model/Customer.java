f9edbd92849af292b35ae7f06e191188cd9e68aa
amirholya01 <amirholya01@gmail.com>
2025-06-14 21:51:11 +0200

    Usig @Jsons Annotations

--- src/main/java/dk/amir/customerservice/model/Customer.java
+++ src/main/java/dk/amir/customerservice/model/Customer.java
@@ -1,11 +1,23 @@
 package dk.amir.customerservice.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import dk.amir.customerservice.enums.CustomerType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+@JsonTypeInfo(include = JsonTypeInfo.As.PROPERTY,
+        use = JsonTypeInfo.Id.NAME,
+        property = "type"
+)
+
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = LegalCustomer.class, name = "LEGAL"),
+        @JsonSubTypes.Type(value = RealCustomer.class, name = "REAL")
+})
 
 @Getter
 @Setter
