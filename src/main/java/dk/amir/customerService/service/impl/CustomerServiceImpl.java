ab7c22c4e925e7c177677cc6c73a5d41e29cf93f
amirholya01 <amirholya01@gmail.com>
2025-06-14 22:50:32 +0200

    implementing Customer Service

--- src/main/java/dk/amir/customerService/service/impl/CustomerServiceImpl.java
+++ src/main/java/dk/amir/customerService/service/impl/CustomerServiceImpl.java
@@ -1,8 +1,12 @@
 package dk.amir.customerService.service.impl;
 
 import dk.amir.customerService.dao.CustomerDao;
+import dk.amir.customerService.enums.CustomerType;
 import dk.amir.customerService.model.Customer;
+import dk.amir.customerService.model.LegalCustomer;
+import dk.amir.customerService.model.RealCustomer;
 import dk.amir.customerService.service.CustomerService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,28 +22,61 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
 
+
+    @PostConstruct
+    public void init() {
+
+        addCustomer(RealCustomer.builder()
+                .id(null)
+                .fullName("John Doe")
+                .phone("001 123 45 67")
+                .email("john@gmail.com")
+                .type(CustomerType.REAL)
+                .nationality("US")
+                .age("42")
+                .build()
+        );
+
+        addCustomer(LegalCustomer.builder()
+                .id(null)
+                .fullName("Stevr Jobs")
+                .phone("001 1234 68 90")
+                .email("steve@app.com")
+                .type(CustomerType.LEGAL)
+                .companyName("Apple")
+                .industry("Tech")
+                .build()
+        );
+    }
     @Override
     public Customer addCustomer(Customer customer) {
-        return null;
+        return customerDao.save(customer);
     }
 
     @Override
     public Customer updateCustomer(Long id, Customer updatedCustomer) {
+        if (customerDao.isExist(id)){
+            return customerDao.save(updatedCustomer);
+        }
         return null;
     }
 
     @Override
     public Customer getCustomerById(Long id) {
-        return null;
+        return customerDao.findById(id);
     }
 
     @Override
     public List<Customer> getAllCustomers() {
-        return List.of();
+        return customerDao.findAll();
     }
 
     @Override
     public boolean deleteCustomer(Long id) {
-        return false;
+        if (customerDao.isExist(id)){
+            customerDao.delete(id);
+            return true;
+        }else
+            return false;
     }
 }
