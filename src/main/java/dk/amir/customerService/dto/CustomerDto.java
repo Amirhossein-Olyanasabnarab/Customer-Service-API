a82cb2d1206a7c2a86e03fdc0ad0db6718d03ca6
amirholya01 <amirholya01@gmail.com>
2025-06-15 08:34:20 +0200

    CustomerDto

---
+++ src/main/java/dk/amir/customerService/dto/CustomerDto.java
@@ -0,0 +1,30 @@
+package dk.amir.customerService.dto;
+
+import dk.amir.customerService.enums.CustomerType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Schema(description = "Customer entity representing a customer service")
+public abstract class CustomerDto {
+
+    @Schema(name = "id", example = "1")
+    private Long id;
+    @Schema(name = "fullName", example = "John Doe")
+    private String fullName;
+    @Schema(name = "email", example = "johnDoe@gmail.com")
+    private String email;
+    @Schema(name = "phone", example = "+1 23 45 67 89")
+    private String phone;
+    @Schema(name = "type", example = "REAL")
+    private CustomerType type;
+}
